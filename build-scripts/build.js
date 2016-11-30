95124968339666dddcf1514f2dcfdd30e34b9d0d
kbeloborodko <kbeloborodko@gmail.com>
2016-11-30 12:32:56 +0200

    Build scripts for production bundles. Webpack configuration file moved to the source.

--- build-scripts/build.js
+++ build-scripts/build.js
@@ -7,7 +7,7 @@ process.env.NODE_ENV = 'production';
 
 console.log(chalk.blue('Generating minified bundle for production. This will take a moment...'));
 
-webpack(webpackConfig).run((err, stats), => {
+webpack(webpackConfig).run((err, stats) => {
   if (err) {
     console.log(chalk.red(err));
     return 1;
